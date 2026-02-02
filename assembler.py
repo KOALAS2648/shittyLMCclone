ca7e1e170da01b6f0d932ae20f3114c595f7012b
KOALAS2648 <107459548+KOALAS2648@users.noreply.github.com>
2026-02-02 15:24:28 +0000

    working version

--- assembler.py
+++ assembler.py
@@ -3,9 +3,22 @@ def readFile(code):
     codeLines = codeFile.readlines()
     codeLines = [i[:-1] if "\n" in i else i for i in codeLines]
     codeLines = [i.split(" ") for i in codeLines]
-    codeLines = [i for i in codeLines if i[0] != "//"]
-    codeLines = [i for i in codeLines if i[0] != '']
+    #codeLines = [i for i in codeLines if i[0] != "//"]
+    #codeLines = [i for i in codeLines if i[0] != '']
+    # remove all the whitespace so oyu can copy and paste directley from LMC
+    for idx, line in enumerate(codeLines):
+        addedLine = []
+        for idx2, word in enumerate(line):
+            match word:
+                case "//":
+                    pass
+                case "":
+                    pass
+                case _:
+                    addedLine.append(word)
+        codeLines[idx] = addedLine
     return codeLines
+        
 
 
 labelsDict = {}
@@ -25,37 +38,41 @@ def getVariables(code, main_functions):
         elif line[0] not in main_functions and line[1] == "DAT":
             variablesDict[line[0]] = index
             code[index].pop(0)
-
-code = readFile("g.txt")
-
-main_functions = {"INP":901, "OUT":902, "HLT":"000", "STA":300, "DAT":"0", "BRA":600, "BRZ":700, "BRP":800, "ADD":100, "SUB":200, "LDA":500, "OTC":400}
-drqInput = ["INP", "OUT", "HLT"]
+            
 
 
+def assemble(codeFile):
+    code = readFile(codeFile)
+    main_functions = {"INP":901, "OUT":902, "HLT":"000", "STA":300, "DAT":"0", "BRA":600, "BRZ":700, "BRP":800, "ADD":100, "SUB":200, "LDA":500, "OTC":400}
+    drqInput = ["INP", "OUT", "HLT"]
 
-loops = ["BRA", "BRP", "BRZ"]
-memory = [0 for i in range(0,99)]
 
-getLabels(code, main_functions)
-getVariables(code, main_functions)
-for index, line in enumerate(code):
 
-    if line[0] in loops:
-        labelname = line[-1]
-        line[-1] = labelsDict[labelname]
+    loops = ["BRA", "BRP", "BRZ"]
+    memory = [0 for i in range(0,99)]
 
-    if line[-1] in variablesDict:
-        varname = line[-1]
-        line[-1] = variablesDict[varname]
-    if line[0] == "OTC":
-        memory[index] = main_functions["OTC"]
-        continue
-    if line[0] in main_functions:
-        if line[0] in drqInput:
-            memory[index] = main_functions[line[0]]
-        else:
-            memory[index] =main_functions[line[0]]+line[-1]
+    getLabels(code, main_functions)
+    getVariables(code, main_functions)
+    for index, line in enumerate(code):
+
+        if line[0] in loops:
+            labelname = line[-1]
+            line[-1] = labelsDict[labelname]
+
+        if line[-1] in variablesDict:
+            varname = line[-1]
+            line[-1] = variablesDict[varname]
+        if line[0] == "OTC":
+            memory[index] = main_functions["OTC"]
+            continue
+        if line[0] in main_functions:
+            if line[0] in drqInput:
+                memory[index] = main_functions[line[0]]
+            else:
+                memory[index] =main_functions[line[0]]+line[-1]
+                
+
+    return memory
 if __name__ == "__main__":
-    print(memory)
-    print(labelsDict)
-    print(variablesDict)
+    c = readFile("code.txt")
+    print(c)
--- runner.py
+++ runner.py
@@ -1,14 +1,17 @@
+from assembler import assemble
 def run(memory):
     counter = 0
     acc = 0
     currentInstruction = None
     index = 0
-    while counter <= 99:
+    while counter < 99:
         #print(memory[counter])
+        #print(counter)
         currentInstruction = str(memory[counter])
         skipAdd = False
         command = int(currentInstruction[0])
         operand = int(currentInstruction[-1])
+        debug = False
         match command:
 
             #(currentInstruction[0])
@@ -17,41 +20,63 @@ def run(memory):
                     counter = 99999
                     continue
                 else:
-                    pass
+                    print("pass")
             case 1: # add
-                
-                acc += int(memory[int(currentInstruction[1:])])
+                address = int(currentInstruction[1:])
+                acc += int(memory[address])
+                if debug:
+                    print(f"add {address}")
             case 2: # sub
+                if debug:
+                    print("sub")
                 acc -= int(memory[int(currentInstruction[1:])])
-            case 3:
+            case 3: # STA / STO
+                if debug:
+                    print("store")
                 index = int(currentInstruction[1:])
-                memory[index] = acc
+                memory[index] = "0"+str(acc)
             case 4:
+                if debug:
+                    print("OTC")
                 print(chr(acc))
             case 5: # LDA
+                if debug:
+                    print("load")
                 data = memory[int(currentInstruction[1:])]
                 acc = int(data)
             case 6: # BRA
+                if debug:
+                    print("branch always")
                 counter = int(currentInstruction[1:])
                 skipAdd = True
             case 7:# BRZ
+                
                 if acc == 0:
                     counter = int(currentInstruction[1:])
-                    
                     skipAdd = True
+                    if debug:
+                        print("branch if 0")
                     
             case 8: # BRP
+                
                 if acc > 0:
                     counter = int(currentInstruction[1:])
                     skipAdd = True
+                    if debug:
+                        print("branch if positive")
             case 9: # handles the input and output
+                
                 if operand == 1:
+                    if debug:
+                        print("input")
                     acc = int(input(">> "))
                 elif operand == 2:
-                    print(acc)
+                    print(f"{acc}")
+
         if not skipAdd:
             counter +=1
     #print(acc)
 
 if __name__ == "__main__":
-    run([510, 313, 513, 400, 111, 313, 212, 709, 602, '000', '032', '01', '0127', '00', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    memory = assemble("code.txt")
+    run(memory)
